e693a986f9dc928381bd8214be21750c5facbd57
Lumin0l <idelaiglesia.m@gmail.com>
2023-09-02 14:58:40 +0200

    portfolio porject - API integrastion

--- ex_02_react_intro/react_intro_project_creation/portfolio-project-current/src/components/portfolio/portfolio-container.js
+++ ex_02_react_intro/react_intro_project_creation/portfolio-project-current/src/components/portfolio/portfolio-container.js
@@ -15,7 +15,6 @@ export default class PortfolioContainer extends Component {
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this);
 
   }
 
@@ -31,7 +30,10 @@ export default class PortfolioContainer extends Component {
     axios
       .get("https://imanoldelaiglesia.devcamp.space/portfolio/portfolio_items")
       .then(response => {
-        console.log("response data", response);
+        console.log("Console Log number 1: response data", response);
+        this.setState({
+          data: response.data.portfolio_items
+        })
       })
       .catch(error => {
         console.log(error);
@@ -40,17 +42,22 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
+      console.log('Console Log number 2: item data:', item);
+      
       return (
-        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
+        <PortfolioItem title={item.name} url={item.url} slug={item.id} />
       );
     });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
   }
 
   render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
-    this.getPortfolioItems();
 
 
     return (
